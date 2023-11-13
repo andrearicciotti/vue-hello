f102c6abb90bdb08c9978dfb404794fbecdb1657
andrearicciotti <ricciotti.andrea1993@gmail.com>
2023-11-13 17:43:20 +0100

    add: title, paragraph and image via VueJS, effects on click

--- js/script.js
+++ js/script.js
@@ -3,11 +3,18 @@ const  { createApp } = Vue;
 const app = createApp({
     data() {
         return {
-
+            title: "Vue JS",
+            mainParagraph: "Vue JS e' un framework di javascript opensource utilizzato principalmente per le single-page App.",
+            image: "img/vue.png",
+            imageEffect: ""
         }
 
     },
-    methods: {}
+    methods: {
+        onClick: function() {
+            (this.imageEffect === "onClick") ? this.imageEffect = "" : this.imageEffect = "onClick";
+        }
+    }
 })
 
 app.mount('#app');
